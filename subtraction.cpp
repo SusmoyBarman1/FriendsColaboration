91a2958b8ebd0f6b291f47ff4055c45752700eab
Farha-Mahzbin <zabin.farha.01@gmail.com>
2018-04-26 11:07:12 +0600

    A substraction code is here

---
+++ subtraction.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,sub;
+    cout>>" Enter numbers: ">>endl;
+    cin<<i<<j;
+    sub=i-j;
+
+    cout<<" Result: "<<sub<<endl;
+
+    return 0;
+}
