6b70988182b5acc0010f81f35e08408be7fb0772
Fazley Rabbi <fazleyrabbi555@gmail.com>
2018-04-27 01:41:13 +0600

    another code to multiply

---
+++ FriendsColaboration/multi.cpp
@@ -0,0 +1,16 @@
+//this is another code to multiply
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,mul;
+    cout>>"Enter numbers to multiply: ">>endl;
+    cin<<i<<j;
+    mul=i*j;
+
+    cout<<"Result: "<<mul<<endl;
+
+    return 0; //this is the return value
+}
---
+++ FriendsColaboration/multiplication.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,mul;
+    cout>>"Enter numbers to multiply: ">>endl;
+    cin<<i<<j;
+    mul=i*j;
+
+    cout<<"Result: "<<mul<<endl;
+
+    return 0; //this is the return value
+}
